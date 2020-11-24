38a02120299f2b53dd48e173be07a5dda1d4b2fc
MRoss <mross20@myseneca.ca>
2020-11-24 13:42:23 -0500

    Set up black and flake8 functionality
     - Updated README and added CONTRIBUTING file
    
     - Created tooling to format and lint project code
    
     - Added VSCode lint and format integration

--- checkMyURL.py
+++ checkMyURL.py
@@ -16,41 +16,60 @@ init()
 def file_parse(filepath, ignoreLinks):
     link_list = []
     try:
-        with open(filepath, 'r') as file_object:
-            for link in bs4.BeautifulSoup(file_object.read(), "html.parser", parse_only=bs4.SoupStrainer('a')):
+        with open(filepath, "r") as file_object:
+            for link in bs4.BeautifulSoup(
+                file_object.read(), "html.parser", parse_only=bs4.SoupStrainer("a")
+            ):
                 linkIgnored = False
                 if ignoreLinks:
                     for iLink in ignoreLinks:
-                        if len(re.findall(iLink,str(link))) != 0: 
+                        if len(re.findall(iLink, str(link))) != 0:
                             linkIgnored = True
-                if link.has_attr('href') and linkIgnored == False:
-                    link_list.append(link['href'])
+                if link.has_attr("href") and linkIgnored is False:
+                    link_list.append(link["href"])
         return link_list
     except OSError:
-        click.secho('Not a valid file (perhaps try --parse_url)', fg='red')
+        click.secho("Not a valid file (perhaps try --parse_url)", fg="red")
         error_code = 5
         return error_code
 
 
-# Takes a file passed to it, and returns the links contained 
+# Takes a file passed to it, and returns the links contained
 def ignore_parse(filepath):
     try:
-        fileText = open(filepath, 'r').read()
-        fileUrl = set(re.findall('(?!# )(http|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', fileText))
-        fileInvalidUrl = set(re.findall('(?!# )(?!http|https)(?!://)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', fileText))
+        fileText = open(filepath, "r").read()
+        fileUrl = set(
+            re.findall(
+                r"(?!# )(http|https)://"
+                r"([\w_-]+(?:(?:\.[\w_-]+)+))"
+                r"([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?",
+                fileText,
+            )
+        )
+        fileInvalidUrl = set(
+            re.findall(
+                r"(?!# )(?!http|https)(?!://)"
+                r"([\w_-]+(?:(?:\.[\w_-]+)+))"
+                r"([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?",
+                fileText,
+            )
+        )
         if fileUrl:
-            fileLink = [(url[0]+"://" + url[1] + url[2]) for url in fileUrl]
+            fileLink = [(url[0] + "://" + url[1] + url[2]) for url in fileUrl]
             return fileLink
         elif fileInvalidUrl:
-            click.secho("The URL provided is invalid. Must begin with https:// or http://",fg='red')
+            click.secho(
+                "The URL provided is invalid. Must begin with https:// or http://",
+                fg="red",
+            )
             error_code = 5
             sys.exit(error_code)
-    except FileNotFoundError as e:
-        click.secho('This file does not exist in the current directory', fg='red')
+    except FileNotFoundError:
+        click.secho("This file does not exist in the current directory", fg="red")
         error_code = 3
         sys.exit(error_code)
-    except IOError as e:
-        click.secho('This file could not be opened', fg='red')
+    except IOError:
+        click.secho("This file could not be opened", fg="red")
         error_code = 3
         sys.exit(error_code)
 
@@ -60,21 +79,21 @@ def ignore_parse(filepath):
 def telescope_parse(link_filter):
     # Can also use the Telescope API: 'https://telescope.cdot.systems/posts/'
     # base_link = 'https://telescope.cdot.systems/posts/'
-    base_link = 'http://localhost:3000/posts/'
+    base_link = "http://localhost:3000/posts/"
     link_list = []
     try:
         req = requests.get(base_link)
-        if (req.status_code != 200):
-            click.secho('Not a valid URL')
+        if req.status_code != 200:
+            click.secho("Not a valid URL")
         else:
             json = req.json()
             for post in json:
-                id = post.get('id')
+                id = post.get("id")
                 url = base_link + id
                 link_list.append(url)
             return link_list
     except requests.exceptions.SSLError:
-        click.secho('An SSL has been encountered', fg='red')
+        click.secho("An SSL has been encountered", fg="red")
 
 
 # Takes a link passed to it, and scrapes the webpage for links
@@ -83,31 +102,33 @@ def link_parse(base_link, link_filter):
     error_code = 0
     try:
         if link_checker(base_link, link_filter)[1] == 200:
-            url_request = urllib.request.Request(base_link, headers={'User-Agent': 'Magic-Browser'})
+            url_request = urllib.request.Request(
+                base_link, headers={"User-Agent": "Magic-Browser"}
+            )
             page = urlopen(url_request)
-            raw_html = page.read().decode('utf-8')
-            html_tags = bs4.BeautifulSoup(raw_html, 'html.parser')
-            for link in html_tags.find_all('a'):
-                if link['href'] == '/':
+            raw_html = page.read().decode("utf-8")
+            html_tags = bs4.BeautifulSoup(raw_html, "html.parser")
+            for link in html_tags.find_all("a"):
+                if link["href"] == "/":
                     link_list.append(base_link)
-                elif link['href'].startswith('/'):
-                    link_list.append(base_link[:-1] + link['href'])
-                elif link['href'].startswith('http'):
-                    link_list.append(link['href'])
+                elif link["href"].startswith("/"):
+                    link_list.append(base_link[:-1] + link["href"])
+                elif link["href"].startswith("http"):
+                    link_list.append(link["href"])
                 else:
-                    format_link = link['href']
+                    format_link = link["href"]
                     link_list.append(format_link[2:-2])
             return error_code, link_list
         else:
-            click.secho('This link is broken', fg='red')
+            click.secho("This link is broken", fg="red")
             error_code = 4
             return error_code
     except requests.exceptions.ConnectionError:
-        click.secho('Link Timeout', fg='red')
+        click.secho("Link Timeout", fg="red")
         error_code = 1
         return error_code
     except requests.exceptions.MissingSchema:
-        click.secho('Not a valid URL (perhaps try --parse_file)', fg='red')
+        click.secho("Not a valid URL (perhaps try --parse_file)", fg="red")
         error_code = 2
         return error_code
 
@@ -117,33 +138,33 @@ def link_parse(base_link, link_filter):
 def link_checker(link, link_filter):
     try:
         response = requests.head(link)
-        response_string = ''
+        response_string = ""
         if response.status_code == 200:
-            if link_filter == 'good' or link_filter == 'all':
-                click.secho('[GOOD]    ' + link, fg='green')
-                response_string = '[GOOD]    ' + link + '\r\n'
+            if link_filter == "good" or link_filter == "all":
+                click.secho("[GOOD]    " + link, fg="green")
+                response_string = "[GOOD]    " + link + "\r\n"
         elif response.status_code == 404 or response.status_code == 400:
-            if link_filter == 'bad' or link_filter == 'all':
-                click.secho('[BAD]     ' + link, fg='red')
-                response_string = '[BAD]     ' + link + '\r\n'
+            if link_filter == "bad" or link_filter == "all":
+                click.secho("[BAD]     " + link, fg="red")
+                response_string = "[BAD]     " + link + "\r\n"
         else:
-            if link_filter == 'unknown' or link_filter == 'all':
-                click.secho('[UNKNOWN] ' + link, fg='white')
-                response_string = '[UNKNOWN] ' + link + '\r\n'
+            if link_filter == "unknown" or link_filter == "all":
+                click.secho("[UNKNOWN] " + link, fg="white")
+                response_string = "[UNKNOWN] " + link + "\r\n"
         return response_string, response.status_code
     except requests.exceptions.ConnectionError:
-        response_string = ''
+        response_string = ""
         status_code = 400
-        if link_filter == 'bad' or link_filter == 'all':
-            click.secho('[ERROR]   ' + link, fg='red')
-            response_string = '[ERROR]   ' + link + '\r\n'
+        if link_filter == "bad" or link_filter == "all":
+            click.secho("[ERROR]   " + link, fg="red")
+            response_string = "[ERROR]   " + link + "\r\n"
         return response_string, status_code
     except requests.exceptions.MissingSchema:
-        response_string = ''
+        response_string = ""
         status_code = 400
-        if link_filter == 'bad' or link_filter == 'all':
-            click.secho('[ERROR]   ' + link, fg='red')
-            response_string = '[ERROR]   ' + link + '\r\n'
+        if link_filter == "bad" or link_filter == "all":
+            click.secho("[ERROR]   " + link, fg="red")
+            response_string = "[ERROR]   " + link + "\r\n"
         return response_string, status_code
 
 
@@ -158,33 +179,71 @@ def list_checker(list, link_filter):
         link_checker(list[0], link_filter)
 
 
-@click.command(context_settings={'ignore_unknown_options': True})
-@click.option('-l', '--parse_link', is_flag=True,
-              help='Will search the link provided for all broken links')
-@click.option('-f', '--parse_file', is_flag=True,
-              help='Will parse the file provided for broken links')
-@click.option('-s', '--save_file', is_flag=True,
-              help='Will parse the file provided for broken links and save results to a text file')
-@click.option('-g', '--good_links', 'link_filter', flag_value='good',
-              help='Filter links by only showing GOOD ones')
-@click.option('-b', '--bad_links', 'link_filter', flag_value='bad',
-              help='Filter links by only showing BAD ones')
-@click.option('-u', '--unknown_links', 'link_filter', flag_value='unknown',
-              help='Filter links by only showing UNKNOWN ones')
-@click.option('-a', '--all_links', 'link_filter', flag_value='all', default=True,
-              help='Show all links')
-@click.option('-i', '--ignore', type=str,
-              help='Use file to filter out unwanted URLS')
-@click.option('-t', '--telescope', 'telescope', is_flag=True,
-              help='Search the 10 most recent posts to Telescope for URLs')
-@click.argument('link')
+@click.command(context_settings={"ignore_unknown_options": True})
+@click.option(
+    "-l",
+    "--parse_link",
+    is_flag=True,
+    help="Will search the link provided for all broken links",
+)
+@click.option(
+    "-f",
+    "--parse_file",
+    is_flag=True,
+    help="Will parse the file provided for broken links",
+)
+@click.option(
+    "-s",
+    "--save_file",
+    is_flag=True,
+    help="Will parse the file provided for broken links"
+    " and save results to a text file",
+)
+@click.option(
+    "-g",
+    "--good_links",
+    "link_filter",
+    flag_value="good",
+    help="Filter links by only showing GOOD ones",
+)
+@click.option(
+    "-b",
+    "--bad_links",
+    "link_filter",
+    flag_value="bad",
+    help="Filter links by only showing BAD ones",
+)
+@click.option(
+    "-u",
+    "--unknown_links",
+    "link_filter",
+    flag_value="unknown",
+    help="Filter links by only showing UNKNOWN ones",
+)
+@click.option(
+    "-a",
+    "--all_links",
+    "link_filter",
+    flag_value="all",
+    default=True,
+    help="Show all links",
+)
+@click.option("-i", "--ignore", type=str, help="Use file to filter out unwanted URLS")
+@click.option(
+    "-t",
+    "--telescope",
+    "telescope",
+    is_flag=True,
+    help="Search the 10 most recent posts to Telescope for URLs",
+)
+@click.argument("link")
 def main(parse_link, parse_file, save_file, link_filter, ignore, telescope, link):
     """
     A tool used to determine if a link provided is a valid link or not.
     Either provide the single link or the file needing parsing.
     Only one filter option is allowed at a time.
 
-    Note: if multiple filters are included, only the last one entered will be applied.
+    Note: if multiple filters are specified, only the last one entered will be applied.
     """
     click.clear()
     if parse_link:
@@ -194,10 +253,10 @@ def main(parse_link, parse_file, save_file, link_filter, ignore, telescope, link
         try:
             ignoreLinks = []
             if save_file:
-                file = open('results.txt', 'w+')
+                file = open("results.txt", "w+")
             if ignore:
                 ignoreLinks = ignore_parse(ignore)
-            link_list = file_parse(link,ignoreLinks)
+            link_list = file_parse(link, ignoreLinks)
             if type(link_list) is int:
                 if save_file:
                     file.close()
@@ -205,7 +264,7 @@ def main(parse_link, parse_file, save_file, link_filter, ignore, telescope, link
                 sys.exit(error_code)
             else:
                 for link in link_list:
-                    if str(link) != 'None':
+                    if str(link) != "None":
                         result = link_checker(link, link_filter)
                         if result[0]:
                             if save_file:
@@ -213,7 +272,7 @@ def main(parse_link, parse_file, save_file, link_filter, ignore, telescope, link
                 if save_file:
                     file.close()
         except FileNotFoundError:
-            click.secho('This file does not exist in the current directory', fg='red')
+            click.secho("This file does not exist in the current directory", fg="red")
             error_code = 3
             sys.exit(error_code)
     elif telescope:
@@ -225,5 +284,5 @@ def main(parse_link, parse_file, save_file, link_filter, ignore, telescope, link
         link_checker(link, link_filter)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
